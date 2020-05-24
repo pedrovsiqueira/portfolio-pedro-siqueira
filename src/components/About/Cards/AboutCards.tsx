20deffb7d67106c3d7f9f19113292453739a3ad2
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-05-24 11:47:42 -0300

    Update About component

--- src/components/About/Cards/AboutCards.tsx
+++ src/components/About/Cards/AboutCards.tsx
@@ -1,41 +1,59 @@
 import React from 'react';
-import { Content, Card } from './styles';
-import { FiUser, FiMail, FiMessageSquare } from 'react-icons/fi';
+import { Content, Card, Container, IconContainer } from './styles';
+import { FiCode } from 'react-icons/fi';
+import { FaFootballBall, FaBookReader, FaDesktop } from 'react-icons/fa';
 
 const AboutCards: React.FC = () => (
   <Content>
-    <Card>
-      <FiUser />
-      <h3>desenvolvimento</h3>
-      <p>
-        Cada dia que passa eu fico encantando com o mundo de desenvolvimento.
-        Por esse motivo estou sempre praticando e estudando.
-      </p>
-    </Card>
-    <Card>
-      <img src="" alt="" />
-      <h3>esportes</h3>
-      <p>
-        Sou fã e praticante de esportes em geral, tais como: futebol, futebol
-        americano, musculação e basquete.
-      </p>
-    </Card>
-    <Card>
-      <img src="" alt="" />
-      <h3>livros</h3>
-      <p>
-        Tenho uma coleção de mais de 200 livros de vários gêneros. Na minha
-        opinião ler é viajar sem sair do lugar.
-      </p>
-    </Card>
-    <Card>
-      <img src="" alt="" />
-      <h3>novas tecnologias</h3>
-      <p>
-        Estou sempre em busca de novos conhecimentos, principalmente quando se
-        trata da área de tecnologia.
-      </p>
-    </Card>
+    <Container>
+      <IconContainer>
+        <FiCode />
+      </IconContainer>
+      <Card>
+        <h3>desenvolvimento</h3>
+        <p>
+          Cada dia que passa eu fico encantando com o mundo de desenvolvimento.
+          Por esse motivo estou sempre praticando e estudando.
+        </p>
+      </Card>
+    </Container>
+    <Container>
+      <IconContainer>
+        <FaFootballBall />
+      </IconContainer>
+      <Card>
+        <h3>esportes</h3>
+        <p>
+          Sou fã e praticante de esportes em geral, tais como: futebol, futebol
+          americano, musculação, corrida e basquete.
+        </p>
+      </Card>
+    </Container>
+    <Container>
+      <Card>
+        <IconContainer>
+          <FaBookReader />
+        </IconContainer>
+        <img src="" alt="" />
+        <h3>livros</h3>
+        <p>
+          Tenho uma pequena coleção de mais de 200 livros de vários gêneros. Na minha
+          opinião ler é viajar sem sair do lugar.
+        </p>
+      </Card>
+    </Container>
+    <Container>
+      <Card>
+        <IconContainer>
+          <FaDesktop />
+        </IconContainer>
+        <h3>novas tecnologias</h3>
+        <p>
+          Estou sempre em busca de novos conhecimentos e desafios, principalmente quando se
+          trata da área de tecnologia.
+        </p>
+      </Card>
+    </Container>
   </Content>
 );
 
