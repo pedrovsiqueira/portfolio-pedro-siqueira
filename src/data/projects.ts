bc40c1fc1fe03b27c8bd674cd50d88645437a6e9
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-05-27 08:12:29 -0300

    Update projects dataset

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,3 +1,51 @@
-const projects = [{}];
+const projects = [
+  {
+    id: 1,
+    img: '',
+    title: 'IMDB - Internet Movies Database Clone',
+    summary: 'Front-end desenvolvido no desafio AceleraDev da Codenation.',
+    tech: ['HTML5', 'CSS3'],
+    demo:
+      'https://pedrovsiqueira.github.io/site-estatico-imdb-clone/public/index.html',
+    repo: 'https://github.com/pedrovsiqueira/site-estatico-imdb-clone',
+  },
+  {
+    id: 2,
+    img: '',
+    title: 'Escape the Bees Game',
+    summary:
+      'Jogo de sobrevivência desenvolvido com VanillaJS e Canvas onde o objetivo é escapar das abelhas.',
+    tech: ['Javascript', 'HTML5', 'CSS3'],
+    demo: 'https://pedrovsiqueira.github.io/javascript-airplane-game/',
+    repo: 'https://github.com/pedrovsiqueira/javascript-airplane-game',
+  },
+  {
+    id: 3,
+    img: '',
+    title: 'Easy Cooking',
+    summary:
+      'Projeto desenvolvido com a Gabriela Aguiar. O objetivo desse projeto é auxiliar as pessoas na hora de cozinhar. Você digita 3 ingredientes que você possui na sua geladeira, e o nosso site retorna receitas compatíveis com os ingredientes informados. É necessário criar uma conta para visualizar e cadastrar novas receitas.',
+    tech: [
+      'Javascript',
+      'NodeJS',
+      'MongoDB',
+      'Handlebars',
+      'Express',
+      'Mongoose',
+    ],
+    demo: 'https://easy-cooking-app.herokuapp.com/',
+    repo: 'https://github.com/pedrovsiqueira/easycooking',
+  },
+  {
+    id: 4,
+    img: '',
+    title: 'Portfólio - Pedro Siqueira',
+    summary:
+      'Portfólio desenvolvido para expor os meus projetos e informações de contato',
+    tech: ['Typescript', 'React', 'HTML5', 'CSS3'],
+    demo: 'http://www.pedrosiqueira.com.br',
+    repo: 'https://github.com/pedrovsiqueira/portfolio-pedro-siqueira',
+  },
+];
 
 export default projects;
