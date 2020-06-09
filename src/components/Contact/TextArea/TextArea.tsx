c4b307026870d4690242eaf25739074d28c44957
William Godoy <william.s.godoy53@gmail.com>
2020-06-09 17:52:34 -0300

    Update TextArea.tsx
    Type useState

--- src/components/Contact/TextArea/TextArea.tsx
+++ src/components/Contact/TextArea/TextArea.tsx
@@ -15,8 +15,8 @@ interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 const TextArea: React.FC<TextAreaProps> = ({ icon: Icon, ...rest }) => {
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [isFilled, setIsFilled] = useState<boolean>(false);
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
