bef57485f73963931e23b7be8dc9591ca602db25
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-26 19:07:28 -0300

    create files for styling variables

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,13 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    salmon: string;
+    orange: string;
+    black: string;
+    white: string;
+    lightSteelBlue: string;
+    cornFlowerBlue: string;
+    red: string;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export default {
+  salmon: '#F2D9D0',
+  orange: '#F2BC8D',
+  black: '#384001',
+  white: '#FAFBFC',
+  lightSteelBlue: '#B0C6D9',
+  cornFlowerBlue: '#54AFDC',
+  red: '#D94A64',
+};
