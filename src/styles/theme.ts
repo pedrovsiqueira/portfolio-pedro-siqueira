619ff65fad9af3c1e95c259130a6c16f4652eff9
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-27 11:06:45 -0300

    start lightTheme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -17,3 +17,23 @@ export const darkTheme = {
   xIconRightNavColor: '#ccc',
   hamburguerIconRightNavColor: '#fff',
 };
+
+export const lightTheme = {
+  backgroundColor: '#272727',
+  textColor: '#FFF',
+  mainColor:
+    'background: -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
+  iconBackgroundColor:
+    'transparent linear-gradient(76deg, #00dcec 0%, #53f3ff 100%) 0% 0% no-repeat padding-box;',
+  buttonColor:
+    'background: transparent -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%) 0% 0% no-repeat padding-box;',
+  inputColor: '#41ebf8',
+  iconColor: '#f4ede8',
+  inputBackgroundColor: '#353535',
+  inputTextColor: '#f4ede8',
+  footerBackgroundColor: '#353535',
+  cardsBackgroundColor: '#353535',
+  rightNavBackgroundColor: '#484848',
+  xIconRightNavColor: '#ccc',
+  hamburguerIconRightNavColor: '#fff',
+};
