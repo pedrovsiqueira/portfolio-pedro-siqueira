83179ab698d60bd1c167618722f8b72d9259048b
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-27 10:57:58 -0300

    refactor css

--- src/components/Projects/Cards/styles.ts
+++ src/components/Projects/Cards/styles.ts
@@ -183,10 +183,7 @@ export const Links = styled.div`
   }
 
   .link-color {
-    background: -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%);
-    -webkit-background-clip: text;
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
+    ${(props) => props.theme.mainColor}
   }
 
   @media screen and (max-width: 480px) {
