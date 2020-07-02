48d0f8ac5858fecdbeef375daa9b250b29a081c7
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-07-01 22:25:14 -0300

    create themes folder

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,23 @@
+export const darkTheme = {
+  backgroundColor: '#272727',
+  textColor: '#FFF',
+  mainColor:
+    'background: -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
+  iconBackgroundColor:
+    'transparent linear-gradient(76deg, #00dcec 0%, #53f3ff 100%) 0% 0% no-repeat padding-box;',
+  buttonColor:
+    'background: transparent -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%) 0% 0% no-repeat padding-box;',
+  inputColor: '#41ebf8',
+  iconColor: '#f4ede8',
+  inputBackgroundColor: '#353535',
+  inputTextColor: '#f4ede8',
+  footerBackgroundColor: '#353535',
+  cardsBackgroundColor: '#353535',
+  rightNavBackgroundColor: '#484848',
+  xIconRightNavColor: '#ccc',
+  hamburguerIconRightNavColor: '#fff',
+  h5TextColor: '#C97310',
+  inputPlaceholderColor: '#BC915E',
+  buttonHover: '#68BBB1',
+  iconHoverColor: '#54A79D',
+};
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,23 +1,3 @@
-export const darkTheme = {
-  backgroundColor: '#272727',
-  textColor: '#FFF',
-  mainColor:
-    'background: -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
-  iconBackgroundColor:
-    'transparent linear-gradient(76deg, #00dcec 0%, #53f3ff 100%) 0% 0% no-repeat padding-box;',
-  buttonColor:
-    'background: transparent -webkit-linear-gradient(82deg, #00dcec 0%, #53f3ff 100%) 0% 0% no-repeat padding-box;',
-  inputColor: '#41ebf8',
-  iconColor: '#f4ede8',
-  inputBackgroundColor: '#353535',
-  inputTextColor: '#f4ede8',
-  footerBackgroundColor: '#353535',
-  cardsBackgroundColor: '#353535',
-  rightNavBackgroundColor: '#484848',
-  xIconRightNavColor: '#ccc',
-  hamburguerIconRightNavColor: '#fff',
-};
-
 export const lightTheme = {
   backgroundColor: '#E1E1E5',
   textColor: '#C97310',
