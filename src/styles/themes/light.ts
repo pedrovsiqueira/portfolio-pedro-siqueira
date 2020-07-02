7f0cfec7a6436c47b5950bcabda4d46f287126e6
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-07-01 22:27:09 -0300

    update light and dark theme files

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,24 +1,27 @@
-export const lightTheme = {
-  backgroundColor: '#E1E1E5',
-  textColor: '#C97310',
-  mainColor:
-    'background: -webkit-linear-gradient(82deg, #54A79D 0%, #65C3B8 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
-  iconBackgroundColor:
-    'transparent linear-gradient(76deg, #54A79D 0%, #65C3B8 100%) 0% 0% no-repeat padding-box;',
-  buttonColor:
-    'background: transparent -webkit-linear-gradient(82deg, #54A79D 0%, #65C3B8 100%) 0% 0% no-repeat padding-box;',
-  inputColor: '#54A79D',
-  iconColor:
-    'background: -webkit-linear-gradient(82deg, #54A79D 0%, #65C3B8 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
-  inputBackgroundColor: '#D8D8DC',
-  inputTextColor: '#C97310',
-  footerBackgroundColor: '#D8D8DC',
-  cardsBackgroundColor: '#D8D8DC',
-  rightNavBackgroundColor: '#3E3E3E',
-  xIconRightNavColor: '#ccc',
-  hamburguerIconRightNavColor: '#fff',
-  h5TextColor: '#C97310',
-  inputPlaceholderColor: '#BC915E',
-  buttonHover: '#68BBB1',
-  iconHoverColor: '#54A79D',
+export default {
+  title: 'light',
+  colors: {
+    backgroundColor: '#E1E1E5',
+    textColor: '#C97310',
+    mainColor:
+      'background: -webkit-linear-gradient(82deg, #54A79D 0%, #65C3B8 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
+    iconBackgroundColor:
+      'transparent linear-gradient(76deg, #54A79D 0%, #65C3B8 100%) 0% 0% no-repeat padding-box;',
+    buttonColor:
+      'background: transparent -webkit-linear-gradient(82deg, #54A79D 0%, #65C3B8 100%) 0% 0% no-repeat padding-box;',
+    inputColor: '#54A79D',
+    iconColor:
+      'background: -webkit-linear-gradient(82deg, #54A79D 0%, #65C3B8 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;',
+    inputBackgroundColor: '#D8D8DC',
+    inputTextColor: '#C97310',
+    footerBackgroundColor: '#D8D8DC',
+    cardsBackgroundColor: '#D8D8DC',
+    rightNavBackgroundColor: '#3E3E3E',
+    xIconRightNavColor: '#ccc',
+    hamburguerIconRightNavColor: '#fff',
+    h5TextColor: '#C97310',
+    inputPlaceholderColor: '#BC915E',
+    buttonHover: '#68BBB1',
+    iconHoverColor: '#54A79D',
+  },
 };
