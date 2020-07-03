f5db2f11836aaf292fc686e046d72e7b35c317a2
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-07-03 19:42:46 -0300

    start translating text to english

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,24 +3,26 @@ import { Container, Content, AboutText } from './styles';
 import SocialIcons from '../SocialIcons/SocialIcons';
 import AboutCards from './Cards/AboutCards';
 import CarouselAboutCards from './CarouselCards/CarouselAboutCards';
+import { FormattedMessage } from 'react-intl';
 
 const About: React.FC = () => {
   return (
     <Container data-testid="container">
       <Content>
         <AboutText>
-          <h5 id="about">sobre mim</h5>
+          <h5 id="about">
+            <FormattedMessage id="about.title" />
+          </h5>
           <h1>
-            olá, sou o <span>Pedro Siqueira</span>
+            <FormattedMessage
+              id="about.message"
+              values={{
+                span: (word: string) => <span>{word}</span>,
+              }}
+            />
           </h1>
           <p data-aos="fade-up" className="about-me">
-            Formado em Ciência da Computação, possuo mais de 8 anos de
-            experiência trabalhando na área de TI. Recentemente decidi migrar
-            para área de programação ao perceber que meu desejo era trabalhar
-            com desenvolvimento. Durante minha formação como Desenvolvedor Web
-            em um Bootcamp da Ironhack tive a oportunidade de aprofundar meus
-            conhecimentos em linguagens e tecnologias como: Javascript, HTML,
-            CSS, React, NodeJS, Express, MongoDB, Git e SCRUM.
+            <FormattedMessage id="about.paragraph" />
           </p>
           <SocialIcons className="social-icons" />
           <AboutCards />
