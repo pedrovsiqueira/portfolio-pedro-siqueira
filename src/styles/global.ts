ca25e3e5caeb7d442a7738244b0444ab6c1b9774
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-04-12 23:15:24 -0300

    feat: add styles for input autocomplete

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -81,4 +81,20 @@ export default createGlobalStyle`
   ::-webkit-scrollbar-button {
     display: none !important;
   }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  textarea:-webkit-autofill,
+  textarea:-webkit-autofill:hover,
+  textarea:-webkit-autofill:focus,
+  select:-webkit-autofill,
+  select:-webkit-autofill:hover,
+  select:-webkit-autofill:focus {
+    border: 1px solid ${(props) => props.theme.colors.buttonColor};
+    -webkit-text-fill-color: ${(props) => props.theme.colors.textColor};
+    box-shadow: 0 0 0px 1000px #${(props) =>
+      props.theme.colors.backgroundColor} inset;
+    transition: background-color 5000s ease-in-out 0s;
+  }
 `;
